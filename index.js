227208a9991d1765765cb41da509b6fa448565d1
nitishmx <nitish7739879577@gmail.com>
2024-06-09 10:19:27 +0530

    post request use to client side to server side request send easily

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 console.log("hye");
 let express = require("express");
 let app = express();
+app.use(express.urlencoded({ extended: true }));//post method ke body ke data find ke liye
+const bodyParser=require('body-parser');
+app.use(bodyParser.json());//specifically parse json data add it to the request.body object
 app.get("/", (req, res) => {
   res.send("first server create");
 });
 
 // route create using express
 app.get('/home',(req,res)=>{
+    // /home (ish route pr get request chalega)
     res.send("this is home route")
 })
 
 // Write post method using express js
 app.post('/api/car',(req,res)=>{
+    // /ap/car (ish route pr post request chalega)
     let {name,brand}=req.body
     console.log(name);
     console.log(brand);
